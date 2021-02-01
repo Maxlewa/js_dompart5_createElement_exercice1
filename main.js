1b3d78a87041486088180a06913764fc7feb92bd
Maxence <lemairemaxence@hotmail.com>
2021-02-01 16:41:58 +0100

    correction

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // EXO 1
 
+let body = document.body
+
 let maDiv = document.querySelector('div#content')
 
 let monH2 = document.createElement('h2');
@@ -17,15 +19,42 @@ maDiv.appendChild(para)
 let monH1 = document.createElement('h1');
 monH1.innerText = 'Le DOM - Création de HTML'
 maDiv.appendChild(monH1)
-maDiv.insertBefore(monH1, maDiv.firstChild)
+maDiv.insertBefore(monH1, monH2)
 
 // EXO 4
 
+let contenuId = maDiv.innerHTML
 let divSecondaire = document.querySelector('#secondaire')
+divSecondaire.innerHTML = contenuId
 
-
-// maDiv.innerHTML = divSecondaire.innerHTML
-
+divSecondaire.querySelector('h2').innerText = 'Part 6 - Exercice 2'
 
 console.log(maDiv)
 console.log(divSecondaire)
+
+// EXO 5
+
+// create div
+
+let idMatiere = document.createElement('div')
+idMatiere.setAttribute('id', 'matiere')
+
+// avoir notre div avant le script 
+
+let myScript = document.querySelector('script')
+body.insertBefore(idMatiere, myScript)
+body.appendChild(idMatiere)
+
+// create list
+
+let myOl = document.createElement('ol')
+idMatiere.appendChild(myOl)
+
+let maMatiere = ["html", "css", "javascript"]
+
+maMatiere.forEach(el => {
+    let myLi = document.createElement('li')
+    myLi.innerText = el
+    myOl.appendChild(myLi)
+})
+
